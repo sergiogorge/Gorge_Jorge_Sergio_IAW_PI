7d58a3db7d40347340e737f0da5f3806001f547b
sergiogorge <sergio1345@gmail.com>
2017-06-09 10:06:28 +0200

    Arreglado borrar valoraciones

--- proyecto/admin/borrar/borrarval.php
+++ proyecto/admin/borrar/borrarval.php
@@ -8,11 +8,6 @@ if ($_SESSION["tipo"]!=='admin'){
 if (empty($_GET))
 die("Tienes que pasar algun parametro por GET.");
 $a = $_GET['id'];
-$connection= new mysqli("localhost", "root", "2asirtriana", "proyecto_blog2");
- if ($connection->connect_errno) {
-   printf("Connection failed: %s\n", $connection->connect_error);
-   exit();
-   }
     if ($result = $connection->query("DELETE FROM valoraciones
      where idnoticia='$a'")) {
       echo "Las valoraciones han sido borradas con éxito.<br>";
@@ -24,3 +19,4 @@ $connection= new mysqli("localhost", "root", "2asirtriana", "proyecto_blog2");
  unset($obj);
  unset($connection);
 ?>
+      
